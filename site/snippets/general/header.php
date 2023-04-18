5cf0f54afb9ff4ded85ffa256108cc6663e221e0
kareldecoeneKDG <info.presentonline@gmail.com>
2023-04-18 21:43:57 +0200

    Fixed title

--- site/snippets/general/header.php
+++ site/snippets/general/header.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Travel - The #1 travel agency in Europe</title>
         <?= css('assets/css/styles.css') ?>
 
         <!-- FONT AWESOME -->
